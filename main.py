a4d3b76e59903a4a892c86437c4fe4ceaac1046d
KALP-1609 <kalp.b0450@gmail.com>
2026-08-09 10:04:48 +0530

    feature: connecting groq api key

---
+++ main.py
@@ -0,0 +1,17 @@
+from dotenv import load_dotenv
+from groq import Groq
+import os
+
+load_dotenv()
+
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+
+response = client.chat.completions.create(
+    model="llama-3.3-70b-versatile",
+    messages = [{
+        "role": "user",
+        "content": "What is the capital of France?"
+    }]
+)
+
+print(response.choices[0].message.content)
