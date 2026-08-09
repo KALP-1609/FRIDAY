8676973e661e3cea4ec2638627baa52b8af19a79
KALP-1609 <kalp.b0450@gmail.com>
2026-08-09 10:14:52 +0530

    fearure: added local memory

--- main.py
+++ main.py
@@ -6,12 +6,26 @@ load_dotenv()
 
 client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
-response = client.chat.completions.create(
-    model="llama-3.3-70b-versatile",
-    messages = [{
-        "role": "user",
-        "content": "What is the capital of France?"
-    }]
-)
+messages = [{
+    "role": "system",
+    "content": "You are FRIDAY, a personal AI assistant."
+}]
 
-print(response.choices[0].message.content)
+print("FRIDAY ONLINE")
+
+while True:
+    user_input = input("You: ")
+
+    if user_input.lower() == "quit" or user_input.lower() == "exit":
+        print("FRIDAY OFFLINE")
+        break
+
+    messages.append({"role": "user", "content": user_input}) # add user input to local memory
+
+    response = client.chat.completions.create(model="llama-3.3-70b-versatile", messages=messages)
+
+    reply = response.choices[0].message.content
+
+    messages.append({"role": "assistant", "content": reply}) # update the local memory with ai response
+
+    print(f"FRIDAY: {reply}") # print response
